75ddf59974f56b4245aa4f6ca6a440bcce1dfc3e
Jun SONG <hj093811@gmail.com>
2024-10-02 16:17:07 +0900

    Thread 코드 추가

--- EXAMPLE/ThreadApp/thread.cpp
+++ EXAMPLE/ThreadApp/thread.cpp
@@ -1,7 +1,34 @@
 #include "thread.h"
 
-Thread::Thread(QObject *parent)
-    : QThread{parent}
+Thread::Thread(QObject *obj) : m_stopFlag(Play)
 {
+    m_label = (QLabel*)obj;
+}
 
+void Thread::run()
+{
+    int cnt = 0;
+    Q_FOREVER {
+        m_mutex.lock();
+        if(m_stopFlag == Stop)
+            m_waitCondition.wait(&m_mutex);
+        m_mutex.unlock();
+
+        //m_label->setText(QString("run %1").arg(cnt++));
+        emit setLabeled(QString("run %1").arg(cnt++));
+        sleep(1);
+    }
+}
+
+void Thread::stopThread()
+{
+    m_stopFlag = Stop;
+}
+
+void Thread::resumeThread()
+{
+    m_mutex.lock();
+    m_stopFlag = Play;
+    m_waitCondition.wakeAll();
+    m_mutex.unlock();
 }
